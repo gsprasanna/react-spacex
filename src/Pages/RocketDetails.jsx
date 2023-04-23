f1e0d22af17f6c0060a73099a2309965ce8ba817
gsprasanna <erprasanna97@gmail.com>
2023-04-24 00:11:05 +0530

    updated the rocket details

--- src/Pages/RocketDetails.jsx
+++ src/Pages/RocketDetails.jsx
@@ -19,8 +19,16 @@ const RocketDetails = () => {
     return (
         <>
             {rocketDetails && <div className="container">
-                <p className="font-bold text-base text-center aboveDesktop:text-lg capitalize mb-3">{rocketDetails?.name}</p>
+                <p className="font-bold text-center text-xl aboveDesktop:text-2xl capitalize mb-3">{rocketDetails?.name}</p>
                 <p>{rocketDetails?.description}</p>
+                <h2 className="font-bold text-lg aboveDesktop:text-2xl my-2">Collections</h2>
+                <div class="grid grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3 hd:grid-cols-3 gap-3">
+                    {
+                        rocketDetails?.flickr_images?.map((item, index) => {
+                            return <img className="w-full h-52" src={item} alt={rocketDetails?.name} key={index} />
+                        })
+                    }
+                </div>
             </div>}
         </>
     );
